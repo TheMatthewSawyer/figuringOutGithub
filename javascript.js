47874a84bb16d58f19d0222890131145fc4127c2
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2019-11-21 20:58:12 -0500

    major code added to every file

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("test").addEventListener("click", function(){
+    document.getElementById("test").value = "CAGE";
+    document.getElementById("text").innerHTML = "CAGE";
+    document.getElementById("text").style.backgroundColor = "green";
+    document.getElementById("image").style.opacity = "1";
+});
