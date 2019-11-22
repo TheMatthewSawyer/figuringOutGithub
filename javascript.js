194d51e9653103ddf1f0a77ea0ddc4ff3e02d1cc
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2019-11-21 23:01:53 -0500

    java and main html updates

--- javascript.js
+++ javascript.js
@@ -3,4 +3,13 @@ document.getElementById("test").addEventListener("click", function(){
     document.getElementById("text").innerHTML = "CAGE";
     document.getElementById("text").style.backgroundColor = "green";
     document.getElementById("image").style.opacity = "1";
+    for(var i = 0; i < 11; i++) {
+        var x = Math.floor(Math.random() * Math.floor(999));
+        var y = Math.floor(Math.random() * Math.floor(999));
+        var z = x.toString() + "/" + y.toString();
+        document.getElementById("image1").src = "https://www.placecage.com/" + z
+        //document.getElementById("image1").src = "https://www.placecage.com/200/200"
+        document.getElementById("image1").style.opacity = "1";
+
+    }
 });
