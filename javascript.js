bf1b85003580961d21074663f15d74b2a7340682
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2019-11-23 11:06:27 -0500

    made the main image change randomly

--- javascript.js
+++ javascript.js
@@ -2,14 +2,11 @@ document.getElementById("test").addEventListener("click", function(){
     document.getElementById("test").value = "CAGE";
     document.getElementById("text").innerHTML = "CAGE";
     document.getElementById("text").style.backgroundColor = "green";
-    document.getElementById("image").style.opacity = "1";
-    for(var i = 0; i < 11; i++) {
-        var x = Math.floor(Math.random() * Math.floor(999));
-        var y = Math.floor(Math.random() * Math.floor(999));
-        var z = x.toString() + "/" + y.toString();
-        document.getElementById("image1").src = "https://www.placecage.com/" + z
-        //document.getElementById("image1").src = "https://www.placecage.com/200/200"
-        document.getElementById("image1").style.opacity = "1";
+    document.getElementById("image1").style.opacity = "0";
+    var x = Math.floor(Math.random() * 899 + 100);
+    var y = Math.floor(Math.random() * 899 + 100);
+    var z = x.toString() + "/" + y.toString();
+    document.getElementById("image1").src = "https://www.placecage.com/" + z
+    document.getElementById("image1").style.opacity = "1";
 
-    }
 });
